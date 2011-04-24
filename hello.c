eac25e0a7d26f109d7abc81e293ff1ef2aea555e
Chenduo <chenduots@163.com>
2011-04-24 15:38:32 +0800

    fi

---
+++ hello.c
@@ -0,0 +1,29 @@
+include <stdio.h>
+void say_hi(void);
+
+int main(int argc, const char *argv[])
+{
+    say_hi();
+    return 0;
+}
+        
+void say_hi(void)
+{
+     printf("hi peter\n");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
